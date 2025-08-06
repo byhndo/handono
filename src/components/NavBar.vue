658562e58642523b3df52ef4f6631740b4fffcae
byhndo <120541041+byhndo@users.noreply.github.com>
2025-08-07 06:48:11 +0700

    Update NavBar.vue

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,13 +1,11 @@
 <script setup>
 import { ref } from 'vue'
-
-const btnNav1 = ref(null)
-const btnNav2 = ref(null)
-
-defineExpose({ btnNav1, btnNav2 }) 
+const { goToBio, goToPhotos, btnNav1, btnNav2 } = defineProps()
 </script>
 
 <template>
+<div>
+  
 <div class="wrapnav">
 <div class="nav theme">
 <button @click="goToBio" ref="btnNav1" class="particles-button">About</button> 
@@ -16,5 +14,7 @@ defineExpose({ btnNav1, btnNav2 })
 <button @click="goToPhotos" ref="btnNav2" class="particles-button">Photo</button> 
 </div>
 </div> 
+
+</div>
 </template>
 
