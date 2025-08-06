a130056a766e23a74bb0ca5647583a7ca8dc780f
byhndo <120541041+byhndo@users.noreply.github.com>
2025-08-06 11:52:24 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,12 @@ import NavBar from './components/NavBar.vue'
 </script>
 
 <template>
+<div :class="['bg', bg]">
   <NavBar />
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
+</div>
+
+
+  <router-view />
 </template>
 
 
