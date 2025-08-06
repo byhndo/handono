a457490685024278c9742432a9b634bbda594fda
byhndo <120541041+byhndo@users.noreply.github.com>
2025-08-06 23:53:10 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -159,10 +159,32 @@ const updateButtonColors = (path) => {
 </svg>
 </div>	
 </div>	
+<main>
+
+<div class="header">
+<div class="indicator">
+<div class="indicator-dot"></div>
+</div>	
+<svg class="hidden">
+  <defs>
+    <filter id="noisetitle">
+      <feGaussianBlur in="SourceGraphic" stdDeviation="0" result="blur"></feGaussianBlur>
+      <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 18 -8" result="goo"></feColorMatrix>
+      <feTurbulence type="fractalNoise" baseFrequency="0.01 1" numOctaves="1" seed="1" result="noise"></feTurbulence>
+      <feDisplacementMap in="goo" in2="noise" scale="0" result="displacement"></feDisplacementMap>
+      <feComposite in="SourceGraphic" in2="displacement" operator="atop"></feComposite>
+      </filter>
+  </defs>
+</svg> 
+<h1>Bayu<br>Handono</h1> 
+<div class="sub-title">Life Sucks and Then You Die!</div>
+</div>	
+	
 <router-view v-slot="{ Component }">
   <transition appear name="slide-fade" mode="out-in" @before-enter="beforeEnter" @after-enter="afterEnter" @after-leave="afterLeave">
     <component :is="Component" :key="$route.fullPath" />
   </transition>
 </router-view>
+</main>
 </div>
 </template>
