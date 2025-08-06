9e60388572dc56eb3f296f9b10a43f5435ea360e
byhndo <120541041+byhndo@users.noreply.github.com>
2025-08-06 16:36:47 +0700

    Update NavBar.vue

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,3 +1,12 @@
+<script setup>
+import { ref } from 'vue'
+
+const btnNav1 = ref(null)
+const btnNav2 = ref(null)
+
+defineExpose({ btnNav1, btnNav2 }) 
+</script>
+
 <template>
 <div class="wrapnav">
 <div class="nav theme">
