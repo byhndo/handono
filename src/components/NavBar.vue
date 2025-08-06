75fad98fba088c04ed68d8a3e3e1ca11d5389310
byhndo <120541041+byhndo@users.noreply.github.com>
2025-08-06 11:26:19 +0700

    Update and rename HelloWorld.vue to NavBar.vue

---
+++ src/components/NavBar.vue
@@ -0,0 +1,11 @@
+<template>
+<div class="wrapnav">
+<div class="nav theme">
+<button @click="goToBio" ref="btnNav1" class="particles-button">About</button> 
+</div>
+<div class="nav theme">
+<button @click="goToPhotos" ref="btnNav2" class="particles-button">Photo</button> 
+</div>
+</div> 
+</template>
+
