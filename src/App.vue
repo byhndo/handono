3230f0a72168e6659f6ac5fceb8e6563134d56b3
byhndo <120541041+byhndo@users.noreply.github.com>
2025-08-06 15:09:53 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -8,8 +8,6 @@ import NavBar from './components/NavBar.vue'
 
 import { ref, watch, onMounted, nextTick } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
-import gsap from 'gsap'
-import { ScrollTrigger } from 'gsap/ScrollTrigger'
 import animePath from './utils/animePath' 
 import preloadImages from './utils/preloadImages' 
 import setupReveal from './utils/setupReveal' 
