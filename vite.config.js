0f723f9a269891c363e0b7f66c9ea2115f6aa81a
byhndo <120541041+byhndo@users.noreply.github.com>
2025-08-06 19:24:38 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   base: '/handono/', 
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),     },
+  }
+}
 })
