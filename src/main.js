3c3f4716809fa41e0a87f01033e9c12329fadaf1
byhndo <120541041+byhndo@users.noreply.github.com>
2025-08-06 15:06:00 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
-import { createApp } from 'vue'
+
+import { createApp, ref, watch, onMounted, nextTick } from 'vue'
+import { createRouter, createWebHashHistory, useRoute, useRouter } from 'vue-router'
 import './style.css'
 import App from './App.vue'
-import router from './router'
+import routes from './router' 
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+app.use(router)
+app.mount('#app')
