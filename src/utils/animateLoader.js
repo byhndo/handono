31ee9c7905c62291c5cc809bab7fc6602a35c044
byhndo <120541041+byhndo@users.noreply.github.com>
2025-08-07 22:10:59 +0700

    Update animateLoader.js

--- src/utils/animateLoader.js
+++ src/utils/animateLoader.js
@@ -26,7 +26,7 @@ function animateValue(element, start, end, duration) {
   }, stepTime);
 }
 
-export default async function animateLoader(onComplete) {
+export default async function animateLoader(done) {
   const perfData = window.performance.timing;
   const estimatedTime = Math.abs(perfData.loadEventEnd - perfData.navigationStart);
   const time = Math.max(Math.floor((estimatedTime / 1000) % 60) * 100, 1200); 
@@ -56,6 +56,7 @@ const tl = gsap.timeline({
     onComplete: () => {     
       contentShow(); 
       ScrollTrigger.refresh();
+      if (typeof done === 'function') done();  
     }
   });
 
