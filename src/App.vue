f6c5f353d288a771c35978e148a52683a7153bd4
byhndo <120541041+byhndo@users.noreply.github.com>
2025-08-07 19:15:16 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -139,7 +139,6 @@ await router.isReady();
   gsap.ticker.lagSmoothing(0);
 await nextTick();
 animateLoader(() => { 
-   contentShow(); 
     updateButtonColors(route.path); 
     triggerAnimation();
     firstLoad.value = false;
