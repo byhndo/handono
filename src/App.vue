c355a2c2f51f1774b8aa46e67277fe553e285963
byhndo <120541041+byhndo@users.noreply.github.com>
2025-08-07 22:19:40 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -56,7 +56,7 @@ const beforeEnter = async (el, done) => {
   done(); 
 }
 
-/*const afterEnter = async (el, done) => {                              
+const afterEnter = async (el, done) => {                              
   await router.isReady();
   await nextTick();
   requestAnimationFrame(() => {
@@ -64,20 +64,8 @@ const beforeEnter = async (el, done) => {
     setupReveal(el);
     done();
   });    
-}; */
-
-const afterEnter = async (el, done) => {
-  await router.isReady();
-  await nextTick();
-
-  animateLoader(() => {
-    setupReveal(el);     
-    done();              
-  });
-};
-
-
-	  
+}; 
+  
 const afterLeave = (el) => {
   if (el.ctx) {
     el.ctx.revert();
