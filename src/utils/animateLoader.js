513a5a4422b6d6e29cc562e58725118570be34b9
byhndo <120541041+byhndo@users.noreply.github.com>
2025-08-07 21:05:22 +0700

    Update animateLoader.js

--- src/utils/animateLoader.js
+++ src/utils/animateLoader.js
@@ -2,7 +2,7 @@ import gsap from 'gsap';
 import SplitText from 'gsap/SplitText';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import Particles from './particles.js'; 
-import contentShow from './contentShow'; 
+import contentShow from './contentShow.js'; 
 
 document.documentElement.style.overflow = 'hidden';
 document.documentElement.style.height = '100%';
