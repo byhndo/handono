7393baecd1a912030d9ebca36c002a0c07354c78
byhndo <120541041+byhndo@users.noreply.github.com>
2025-08-08 09:20:50 +0700

    Update afterPreload.js

--- src/utils/afterPreload.js
+++ src/utils/afterPreload.js
@@ -1,9 +1,6 @@
 import gsap from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import contentShow from './contentShow.js';
 import setupReveal from './setupReveal.js';
-
-gsap.registerPlugin(ScrollTrigger); 
 
 export default function afterPreload() {
   contentShow();
