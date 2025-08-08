100aabeb7a1fe77e299f0a0cc2826a702e3f0bef
byhndo <120541041+byhndo@users.noreply.github.com>
2025-08-08 11:30:27 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -108,8 +108,8 @@ const triggerAnimation = () => {
   animePath(bg.value);      
 };
 
-onMounted(async() => {	
-await router.isReady();
+onMounted(() => {	
+router.isReady();
   if (firstLoad.value && route.path !== '/bio') {
     await router.push('/bio');
     bg.value = 'bio';
@@ -138,12 +138,11 @@ await router.isReady();
 
   gsap.ticker.lagSmoothing(0);
 
-	await nextTick();
-
-animateLoader(() => {  	  
+nexTick(() => {  	  
     updateButtonColors(route.path);
     triggerAnimation();
     firstLoad.value = false; 
+	animateLoader();
   });	  
 });
 
