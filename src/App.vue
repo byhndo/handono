c9e963df7616c8fc8de7c01515a97142e572c538
byhndo <120541041+byhndo@users.noreply.github.com>
2025-08-08 23:24:45 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -105,7 +105,7 @@ onMounted(async() => {
   }
 
   const lenis = new Lenis({
-    duration: 2,
+    duration: 2.3,
     easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
     direction: 'vertical',
     gestureDirection: 'vertical',
