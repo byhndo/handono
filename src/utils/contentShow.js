316136279264a88509ca295f4ca5c01ac9123d68
byhndo <120541041+byhndo@users.noreply.github.com>
2025-08-10 00:29:14 +0700

    Update contentShow.js

--- src/utils/contentShow.js
+++ src/utils/contentShow.js
@@ -68,6 +68,35 @@ gsap.set(".sub-title", {
   opacity:1
 });
 
+gsap.set('.indicator', {
+ y:-100	
+});
+	
+gsap.to('.indicator', {
+ y:0,
+ duration:dur,
+ autoAlpha: 1,
+ ease: easing,
+ onComplete: () => {
+ gsap.fromTo('.indicator', {
+  autoAlpha: 1,
+  y:0,
+  ease: easing
+ }, {
+  autoAlpha: 0,
+  y:-100,
+  duration: dur,
+  ease: easing,
+  scrollTrigger: {
+  scrub: 3,
+  trigger: '.indicator',
+  start: 'center 40%',
+  end: 'center 70%'					
+  }
+ });
+}
+}, "+=2");
+
 gsap.to('.header', {  
  onComplete: () => {
   gsap.to(".header", {  
@@ -204,34 +233,6 @@ bttn.addEventListener("click", () => {
   });
 })();
 
-gsap.set('.indicator', {
- y:-100	
-});
-	
-gsap.to('.indicator', {
- y:0,
- duration:dur,
- autoAlpha: 1,
- ease: easing,
- onComplete: () => {
- gsap.fromTo('.indicator', {
-  autoAlpha: 1,
-  y:0,
-  ease: easing
- }, {
-  autoAlpha: 0,
-  y:-100,
-  duration: dur,
-  ease: easing,
-  scrollTrigger: {
-  scrub: 3,
-  trigger: '.indicator',
-  start: 'center 40%',
-  end: 'center 70%'					
-  }
- });
-}
-}, "+=2");
                     
 			 
 }
