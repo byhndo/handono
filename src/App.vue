03eb590251b00792104239bf7fd8997081cceff9
byhndo <120541041+byhndo@users.noreply.github.com>
2025-08-09 22:17:00 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -95,6 +95,10 @@ const goToPhotos = () => {
 
 onMounted(async() => {
 await router.isReady();
+  if (firstLoad.value && route.path !== '/bio') {
+    await router.replace('/bio');
+    bg.value = 'bio';
+}
 
 const lenis = new Lenis({
   duration: 2,
