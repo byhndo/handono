f8084ad0702be2813825ce2fdcf4240676584db9
byhndo <120541041+byhndo@users.noreply.github.com>
2025-08-09 14:05:51 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,36 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'path' 
+import path from 'path'
 
 export default defineConfig({
   base: '/handono/',
-  plugins: [vue()],
+  plugins: [
+    vue(),
+
+      {
+      name: 'replace-vite-favicon',
+      transformIndexHtml(html) {
+        
+        let cleaned = html.replace(
+          /<link rel="icon"[^>]*vite\.svg[^>]*>/,
+          ''
+        )
+      
+        cleaned = cleaned.replace(
+          '</head>',
+          `  <link rel="icon" type="image/png" href="/favicon.png?v=${Date.now()}">\n</head>`
+        )
+
+        return cleaned
+      }
+    }
+  ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src') 
+      '@': path.resolve(__dirname, './src')
     }
   },
   build: {
-    sourcemap: true, 
+    sourcemap: true
   }
 })
