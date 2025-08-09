e97efd08e8ce3a84d9b2255d485758cbb415ad1e
byhndo <120541041+byhndo@users.noreply.github.com>
2025-08-09 14:41:21 +0700

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,36 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'path'
+import path from 'path' 
 
 export default defineConfig({
   base: '/handono/',
-  plugins: [
-    vue(),
-
-      {
-      name: 'replace-vite-favicon',
-      transformIndexHtml(html) {
-        
-        let cleaned = html.replace(
-          /<link rel="icon"[^>]*vite\.svg[^>]*>/,
-          ''
-        )
-      
-        cleaned = cleaned.replace(
-          '</head>',
-          `  <link rel="icon" type="image/png" href="/favicon.png?v=${Date.now()}">\n</head>`
-        )
-
-        return cleaned
-      }
-    }
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
+      '@': path.resolve(__dirname, './src') 
     }
   },
   build: {
-    sourcemap: true
+    sourcemap: true, 
   }
 })
